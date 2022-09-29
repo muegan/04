ffcfe7e8b08e832129bc5d2b07447ec016d75a62
muegan <2216869@sookmyung.ac.kr>
2022-09-29 16:40:27 +0900

    2번

--- main.c
+++ main.c
@@ -4,19 +4,14 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
+	int a,b;
 	
-	int x,y,z,m;
-	int a,b,c;
-	x=1;
-	y=3;z=5;
-	a=3;
-	b=4;
-	c=5;
+	scanf("%i%i",&a,&b);
 	
-	y=a*x*x+b*x+c;
-	m=(x+y+z)/3;
-	
-	printf("y=%d, m=%d",y,m);
+	printf("+ result is %d\n",a+b);
+	printf("- result is %d\n",a-b);
+	printf("* result is %d\n",a*b);
+	printf("/ result is %d\n",a/b);
 	
 	return 0;
 }
